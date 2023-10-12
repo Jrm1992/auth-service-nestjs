11e65e7b30b0d457db52a0e73a6a340436a38392
Jrm1992 <jose_roberto_6@msn.com>
2023-10-12 20:49:19 -0300

    feat(auth): implement login generating JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
 
@@ -22,7 +29,17 @@ export class AuthService {
     throw new UnauthorizedException('Invalid email or password');
   }
 
-  login() {
-    throw new Error('Method not implemented.');
+  login(user: User): UserToken {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+    };
   }
 }
